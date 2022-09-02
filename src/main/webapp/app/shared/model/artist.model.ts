9aa05e2915e5eb0ff2f5e711b417f55159623903
eugeneshine1 <eugeneshine1@gmail.com>
2022-09-02 10:47:21 -0400

    reset

--- src/main/webapp/app/shared/model/artist.model.ts
+++
@@ -1,9 +0,0 @@
-import { ISong } from 'app/shared/model/song.model';
-
-export interface IArtist {
-  id?: number;
-  name?: string;
-  songs?: ISong[] | null;
-}
-
-export const defaultValue: Readonly<IArtist> = {};
